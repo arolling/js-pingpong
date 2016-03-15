8a86e96dde282b59f4ab88b57d208847a8969485
Abigail Rolling and Isabelle George <10izzygeorge@gmail.com>
2016-03-15 15:50:36 -0700

    Add sass to project

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,8 @@ var lib = require('bower-files')({
   }
 });
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('bowerJS', function () {
   return gulp.src(lib.ext('js').files)
@@ -60,6 +62,7 @@ gulp.task("minifyScripts", ["jsBrowserify"], function(){
     .pipe(gulp.dest("./build/js"));
 });
 
+
 gulp.task("clean", function(){
   return del(['build', 'tmp']);
 });
@@ -71,6 +74,16 @@ gulp.task("build", ['clean'], function(){
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
+  gulp.start('cssBuild');
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
@@ -96,4 +109,5 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
 });
